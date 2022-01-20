245a90e73deb6ca7d6112b3e8c4e64603d4eb128
ejohn162 <95258460+ejohn162@users.noreply.github.com>
2022-01-19 19:07:53 -0700

    updated js functions

--- src/app.js
+++ src/app.js
@@ -1,10 +1,22 @@
 function displayTemperature(response){
+    
+    let temperatureElement= document.querySelector("#temperature");
+    temperatureElement.innerHTML=Math.round(response.data.main.temp);
+    let cityElement=document.querySelector("#city");
+    cityElement.innerHTML=response.data.name;
+    let descriptionElement= document.querySelector("#description");
+    descriptionElement.innerHTML=response.data.weather[0].description;
+    let humidityElement=document.querySelector("#humidity");
+    humidityElement.innerHTML=response.data.main.humidity;
+    let windElement=document.querySelector("#wind");
+    windElement.innerHTML=response.data.wind.speed;
     console.log(response.data);
+
 
 }
 
 let apiKey = "803207aaaf3a18c2e1d94a41af0a3491";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 
 console.log(apiUrl);
 
